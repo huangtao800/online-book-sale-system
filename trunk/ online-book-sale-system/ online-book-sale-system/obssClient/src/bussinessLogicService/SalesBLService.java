a4d085e74dedd409a80a0e5c1f1bd21fa9898271
mzwaverly <mzwaverly@b57b6b14-39f2-756c-e018-90b6af572c45>
2012-10-18 12:43:32 +0000

    张雅婷补充

---
+++ trunk/ online-book-sale-system/ online-book-sale-system/obssClient/src/bussinessLogicService/SalesBLService.java
@@ -0,0 +1,19 @@
+package bussinessLogicService;
+
+import java.util.ArrayList;
+
+import po.OrderPO;
+import po.ResultMessage;
+
+
+
+public interface SalesBLService {
+	public ResultMessage putInCart(String id);//把商品加入购物车，返回添加成功
+	public ArrayList<String> enterCart();//进入购物车，显示购物车信息
+	public ResultMessage removeFromCart(String id);//进入购物车之后，进行删除，结束后要刷新
+	public double calculate();//计算总价
+	public double chooseCoupon (double percent);//选择打折券
+	public double chooseVoucher (double voucher);//选择等价券
+	public OrderPO purchase();//生成订单
+	public void endSale ();//结束整个购买 不会写！！！！！！！！！！！
+}
