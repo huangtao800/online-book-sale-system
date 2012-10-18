041d7adce1d24fac12cfad5dad6b74e41b969a40
mzwaverly <mzwaverly@b57b6b14-39f2-756c-e018-90b6af572c45>
2012-10-18 13:05:51 +0000

    张雅婷修改

--- trunk/ online-book-sale-system/ online-book-sale-system/obssClient/src/po/OrderPO.java
+++ trunk/ online-book-sale-system/ online-book-sale-system/obssClient/src/po/OrderPO.java
@@ -1,5 +1,14 @@
 package po;
 
 public class OrderPO implements PO{
+	static long count = 00000;
+	private long orderNum;
+	public OrderPO(){
+		orderNum = count ++;
+	}
+	public long getOrderNum(){
+		return orderNum;
+	}
+
 
 }
