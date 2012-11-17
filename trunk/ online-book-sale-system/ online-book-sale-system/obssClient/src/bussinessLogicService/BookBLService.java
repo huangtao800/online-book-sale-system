fb1ac8c3374f9935e839862c57a489389fdbb0de
youjiaqinju <youjiaqinju@b57b6b14-39f2-756c-e018-90b6af572c45>
2012-11-17 07:38:55 +0000

    as

--- trunk/ online-book-sale-system/ online-book-sale-system/obssClient/src/bussinessLogicService/BookBLService.java
+++ trunk/ online-book-sale-system/ online-book-sale-system/obssClient/src/bussinessLogicService/BookBLService.java
@@ -7,11 +7,14 @@ public interface BookBLService {
 	//销售界面查询图书和图书的促销信息
 	public ArrayList<BookPO> findByKey(String name);//根据关键字返回相应的图书列表
 	public ArrayList<BookPO> findByType(String type);//选择图书类别，返回相应的图书列表
-	public ArrayList<PromotionPO> getBookPromotionListByID(String bookID);//返回相应图书的促销信息
+	public BookPO findByISBN(String ISBN);//通过ISBN来查找图书
 	
 	//管理图书（增，删，改）
 	public ResultMessage addBook(BookPO b);
 	public ResultMessage deleteBook(String id);
 	public ResultMessage modifyBook(String id);
+	
+	//刷新图书信息
+	public ResultMessage updateBook(ArrayList<LineItemPO> salesList);
 
 }
