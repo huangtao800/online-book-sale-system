c3368b28a9dec230bc7c9680176dfb070a756251
youjiaqinju <youjiaqinju@b57b6b14-39f2-756c-e018-90b6af572c45>
2012-11-17 07:38:30 +0000

    d

---
+++  online-book-sale-system/ online-book-sale-system/obssClient/src/bussinessLogic/domain/MockUser.java
@@ -0,0 +1,11 @@
+package bussinessLogic.domain;
+
+import po.UserPO;
+
+public class MockUser extends User{
+
+	public MockUser(String name, String password, String id) {
+		super(name, password, id);
+	}
+    
+}
