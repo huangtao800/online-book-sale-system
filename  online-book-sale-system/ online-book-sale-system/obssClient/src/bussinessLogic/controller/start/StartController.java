53f8d4c30b52b6569a907b8d924c30783c963e2d
huangtao800 <huangtao800@b57b6b14-39f2-756c-e018-90b6af572c45>
2012-12-05 09:19:09 +0000

    StartController移到专门的start包中

---
+++  online-book-sale-system/ online-book-sale-system/obssClient/src/bussinessLogic/controller/start/StartController.java
@@ -0,0 +1,58 @@
+package bussinessLogic.controller.start;
+
+import javax.swing.JOptionPane;
+
+import po.ResultMessage;
+import po.UserPO;
+import po.UserRole;
+import presentation.RegistryView;
+import presentation.StartView;
+import bussinessLogic.controller.UserController;
+import bussinessLogicService.RegistryBLService;
+import bussinessLogicService.StartBLService;
+import bussinessLogicService.UserBLService;
+
+public class StartController implements StartBLService{
+	StartView startView;
+	UserBLService userController;
+	RegistryBLService registryController;
+	
+
+	//构造函数应传入Model
+	public StartController(){
+		userController=UserController.getInstance();
+		startView=new StartView(this);
+		startView.setVisible(true);
+	}
+	
+	@Override
+	public void logIn(String name, String password,UserRole role) {
+		// TODO Auto-generated method stub
+		UserPO userPO=userController.login(name, password, role);
+		
+		if(userPO!=null){
+			startView.setVisible(false);
+			startView.dispose();
+			System.out.println("success!");
+			initNewView(role);
+		}else {
+			JOptionPane.showMessageDialog(null, "对不起！用户名或密码错误！");
+		}
+	}
+	
+	private void initNewView(UserRole role){
+		if(role==UserRole.Member){
+			
+		}else if(role==UserRole.SalesManager){
+			
+		}
+	}
+
+
+	@Override
+	public void startRegistry() {
+		// TODO Auto-generated method stub
+		registryController=new RegistryController();
+	}
+
+}
